399b91a4de05194f9053a1f3e62af967980ed160
Trisha Reddy <105570654+trishavreddy@users.noreply.github.com>
2023-11-17 11:51:11 -0500

    Added timer

--- Runner.java
+++ Runner.java
@@ -6,6 +6,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import java.time.Duration;
+import java.time.Instant;
+
 
 public class Runner {
 
@@ -13,6 +16,7 @@ public class Runner {
 
     public static void main(String[] args)
     {
+        Instant startTime = Instant.now();
         try{
 
             ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
@@ -28,7 +32,10 @@ public class Runner {
                 sum+=fut.get();
             }
             executor.shutdown();
+            Instant endTime = Instant.now();
+            Duration interval = Duration.between(startTime, endTime);
             System.out.println("Sum: "+sum);
+            System.out.println("Time taken: "+interval.toMillis()+" milliseconds");
 
         }catch(InterruptedException|ExecutionException ex){
             System.out.println(ex.getMessage());
