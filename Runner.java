405fa7dcfede06a7287ea026597d928d6ced34e2
Trisha Reddy <105570654+trishavreddy@users.noreply.github.com>
2023-11-17 05:25:35 -0500

    Wrote virtual threads, executor service

--- Runner.java
+++ Runner.java
@@ -1,10 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+
 public class Runner {
 
-    
+    public static int sum = 0;
 
     public static void main(String[] args)
     {
-        
+        try{
+
+            ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+
+            List<MyThread> threads = new ArrayList<>();
+            for(int i = 0; i < 100; i++)
+            {
+                threads.add(new MyThread());
+            }
+            List<Future<Integer>> list = executor.invokeAll(threads);
+            for(Future<Integer> fut : list)
+            {
+                sum+=fut.get();
+            }
+            executor.shutdown();
+            System.out.println("Sum: "+sum);
+
+        }catch(InterruptedException|ExecutionException ex){
+            System.out.println(ex.getMessage());
+        }
     }
-    
+
 }
